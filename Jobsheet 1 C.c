69fb49d180d28a73bb7a9dee14d10f637951b9ac
AdeSetianKusumaWijaya <64572059+AdeSetianKusumaWijaya@users.noreply.github.com>
2020-05-12 11:09:16 +0800

    Create Jobsheet 1 C.c
    Buatlah program array satu dimensi yang menampilkan NIM masing – masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std;
+main(){
+	printf ("Nama     : Ade Setian Kusuma Wijaya");
+	printf ("\nNIM      : F1B019006");
+	printf ("\nKelompok : 02\n\n");
+	
+	int nim [6] = {2, 5, 7, 3, 6, 1};
+	printf ("Data ke 1 : %d\n", nim [0]);
+	printf ("Data ke 2 : %d\n", nim [1]);
+	printf ("Data ke 3 : %d\n", nim [2]);
+	printf ("Data ke 4 : %d\n", nim [3]);
+	printf ("Data ke 5 : %d\n", nim [4]);
+	printf ("Data ke 6 : %d", nim [5]);
+}
